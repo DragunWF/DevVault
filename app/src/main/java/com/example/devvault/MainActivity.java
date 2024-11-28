2b192079bf40c2ee48867302fde5f3ac1461e922
DragunWF <plarisanmarc@gmail.com>
2024-11-28 11:49:55 +0800

    update backend

--- app/src/main/java/com/example/devvault/MainActivity.java
+++ app/src/main/java/com/example/devvault/MainActivity.java
@@ -36,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Test
         try {
             searchView = findViewById(R.id.searchView);
             capsuleRecyclerView = findViewById(R.id.capsuleRecyclerView);
